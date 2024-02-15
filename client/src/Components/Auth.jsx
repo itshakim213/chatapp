232a9229cef26e76c0dad03a1e980a40a644c8ff
itshakim213 <hikhlef4@gmail.com>
2024-02-15 16:17:17 +0100

    server deployed succesfully

--- client/src/Components/Auth.jsx
+++ client/src/Components/Auth.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
+      .post("https://chat-server-vq9u.onrender.com/authenticate", { username: value })
       .then((r) => props.onAuth({ ...r.data, secret: value }))
       .catch((e) => console.log("Auth Error", e));
   };
